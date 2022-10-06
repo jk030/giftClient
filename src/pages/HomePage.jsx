c0f6adc6a4cb94a90a85a770282db0c352e9363a
paulifauli <--global>
2022-10-06 11:01:27 +0200

    paar schöhnheitssachen

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,18 +1,18 @@
 import "../App.css";
-// import React from "react";
-// import axios from "axios";
-// import 
 import React from "react"
-import {useState, useEffect} from "react"
+import {useState, useEffect, useContext} from "react"
 import axios from "axios"
 import {Link} from "react-router-dom"
-//import RecipientCard from "../components/RecipientCard"
+import { AuthContext } from "../context/auth.context";
+
 
 function HomePage() {
   const [recipientInfo, setRecipientInfo] = useState([]); 
-  console.log(recipientInfo)
   const [filteredRecipient, setFilteredRecipient] = useState([]); 
   const [search, setSearch] = useState ("")
+  const { isLoggedIn, logOutUser, user } = useContext(AuthContext);
+  
+  
   const handleSearch = (e) => setSearch(e.target.value);
 
    const getRecipientInfo = () => {
@@ -48,38 +48,39 @@ function HomePage() {
   return (
     <div>
         <img className="imgHomePage" src="/Img/mia-golic-6JtuGvLzh20-unsplash.jpg" alt="gift"/> 
-          <div  className="HomePage-Container"> 
-          <p className="headlineHomePage">You're a last minute shopper? Always late with buying presents? Everytime the same hustel? <br/>Use GoodTimes, collect ... </p>
-          <a href="/signup"><button className="signUpbtn">Create account</button></a>
-          </div>
+        <div  className="HomePage-Container"> 
+            <p className="headlineHomePage">You're a last minute shopper? Always late with buying presents? Everytime the same hustel? <br/>Use GoodTimes, collect ... </p>
+            {!isLoggedIn && <a href="/signup"><button className="signUpbtn">Create account</button></a>}
+        </div>
         
         <div className="button-container">
             <a href="#search">
               <div className="button-down"></div>
-              </a>
+            </a>
         </div>
           
           
 
       <div className="searchForm" id="search"> 
+
           <form className="form" >
           <label className="headlineSearchForm">Search for public Lists:</label>
               <input id="search" type="text" name="search" value={search} placeholder="Who are you looking for ?" onChange={handleSearch}/> 
             {filteredRecipient.length !== 0 && filteredRecipient.map((info) => {
             return (
-              <ul class="cards"> 
+              <ul className="cards" key={info?._id}> 
                 <li> 
-                    <a href={`/listPage/${info?._id}`} class="card" > 
-                    <div class="card__background"> This List is for: <br /> {info?.name} </div>
+                    <a href={`/listPage/${info?._id}`} className="card" > 
+                    <div className="card__background"> This List is for: <br /> {info?.name} </div>
                     {/* <img src={info?.imageRecipient} class="card__image" alt="" alt="Recipient" /> */}
-                      <div class="card__overlay" key={info?._id}> 
-                            <div class="card__header">
-                              <svg class="card__arc" ><path /></svg>  
+                      <div className="card__overlay" key={info?._id}> 
+                            <div className="card__header">
+                              <svg className="card__arc" ><path /></svg>  
                               {/* xmlns="http://www.w3.org/2000/svg" */}
                               
-                                  <div class="card__header-text">  
-                                  <img src={info?.imageRecipient} class="card__thumb"  alt="Recipient" />
-                                      {info?.user?.userName && <h4 class="card__status"> Created by: {info?.user?.userName}</h4>}
+                                  <div className="card__header-text">  
+                                  <img src={info?.imageRecipient} className="card__thumb"  alt="Recipient" />
+                                      {info?.user?.userName && <h4 className="card__status"> Created by: {info?.user?.userName}</h4>}
                                   </div>
                                   <Link to={`/listPage/${info?._id}`}> <button className="signUpbtn">See Gift List</button> </Link>  
                                   <Link to={`/profilePage/${info?.user?._id}`}> <button className="signUpbtn"> See {info?.user?.userName}'s Profile</button> </Link> 
@@ -88,10 +89,10 @@ function HomePage() {
                     </a>
                 </li>
               </ul>
-            )
-      })}
-        </form>
-    </div>
+              )})}
+
+          </form>
+      </div>
     </div>
   );
 }
